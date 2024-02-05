ee3b048a42808a4efa2b744692a95fc1224bf07b
jlee600 <jinseo775@gmail.com>
2024-02-04 19:48:10 -0500

    made buttons interactive

--- app/src/main/java/com/example/gatechmanager/MainActivity.java
+++ app/src/main/java/com/example/gatechmanager/MainActivity.java
@@ -1,15 +1,45 @@
 package com.example.gatechmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gatechmanager.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new scheduleFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.schedule) {
+                replaceFragment(new scheduleFragment());
+            } else if (itemId == R.id.add) {
+                replaceFragment(new addFragment());
+            } else if (itemId == R.id.todo) {
+                replaceFragment(new todoFragment());
+            }
+
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
 }
-//merge test
+
--- app/src/main/java/com/example/gatechmanager/scheduleFragment.java
+++ app/src/main/java/com/example/gatechmanager/scheduleFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +59,9 @@ public class scheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("scheduleFragment", "onCreateView called");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_schedule, container, false);
     }
+    // Add this to onBottomNavigationItemSelected method in MainActivity
 }
