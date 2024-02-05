1db0051eb5d3b233fb1a4b072173967edce78fc7
jlee600 <jinseo775@gmail.com>
2024-02-04 21:32:29 -0500

    commit before merging

--- app/src/main/java/com/example/gatechmanager/MainActivity.java
+++ app/src/main/java/com/example/gatechmanager/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
-
 import com.example.gatechmanager.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
