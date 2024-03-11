e1154ea5f1dfc36140bcd72694327b8f49a290d1
jlee600 <jinseo775@gmail.com>
2024-03-10 20:39:49 -0400

    schedule: add btn

--- app/src/main/java/com/example/gatechmanager/scheduleFragment.java
+++ app/src/main/java/com/example/gatechmanager/scheduleFragment.java
@@ -1,8 +1,32 @@
 package com.example.gatechmanager;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
-public class scheduleFragment extends Fragment {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class scheduleFragment extends Fragment {
+    public scheduleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        FloatingActionButton addButton = view.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle FloatingActionButton click
+            }
+        });
+
+        return view;
+    }
 }
 
